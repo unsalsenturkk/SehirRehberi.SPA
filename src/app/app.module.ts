fb840305f5cf961ef6d71a51509a693e8d200091
unsalsenturkk <unsalsenturkk@gmail.com>
2021-12-15 15:30:08 +0300

    Routing Mimarisinin Kurulması

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -16,7 +18,8 @@ import { CityComponent } from './city/city.component';
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
