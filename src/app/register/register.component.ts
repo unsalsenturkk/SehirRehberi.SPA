5c09f1e768ed665bc1dac524d5af353177ef9781
unsalsenturkk <unsalsenturkk@gmail.com>
2021-12-20 17:37:32 +0300

    Register Formunun Görselleştirilmesi

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -37,7 +37,7 @@ export class RegisterComponent implements OnInit {
   }
   
   passwordMatchValidator(g : FormGroup){
-    return g.get('password')?.value === g.get('confirmPassword')?.value ? null : {missMatch:true}
+    return g.get('password')?.value === g.get('confirmPassword')?.value ? null : {missmatch:true}
   }
 
   register(){
